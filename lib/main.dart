323963a8937f06a59513c676a4f652e4877e7dc2
Salov Konstantin <ferrum9696@mail.ru>
2021-08-05 14:15:48 +0300

    Инициализация игры

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
-import 'package:flutter_game/game_Core/main_loop.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  startLoop();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Game"),
+      ),
+    ));
+  });
 }
